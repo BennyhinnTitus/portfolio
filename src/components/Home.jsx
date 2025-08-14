ac132ce8526e9dc1bc8cb76ffa1ce4bac5cd0e18
BennyhinnTitus <bennyhinntitus@gmail.com>
2025-08-14 23:55:01 +0530

    Created the home components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,34 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import socialLinks from '../utils/socialLinks';
+
 const Home = () => {
+    const mappedSocialLinks = socialLinks.map(link => {
+        return (
+        <button 
+            className='link-btn'
+            onClick={() => window.open(link.path , "_blank")}
+        >
+            <FontAwesomeIcon 
+                className='social-links'
+                icon={link.icon}
+                shake
+                size='2x'
+                style={{ color: "rgb(97, 32, 172)" }}
+            />
+        </button>
+        )
+    })
+
     return (
-        <div className="Home"></div>
+        <div className="Home">
+            <div className="content">
+                <span className="name">Bennyhinn Titus D</span>
+                <span>Security Engineer | Web Developer</span>
+                <p className="intro">I'm a Cybersecurity undergraduate with a passion for crafting secure, modern web experiences. I love turning ideas into interactive websites and constantly exploring new tech to sharpen my skills.</p>
+                <div className="links-container">{mappedSocialLinks}</div>
+            </div>
+            <div className="animation-container">Can't find an animation...</div>
+        </div>
     )
 }
 
