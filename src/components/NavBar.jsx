34e77596331c07f0f6be17e8c9af0283a9cca1cd
BennyhinnTitus <bennyhinntitus@gmail.com>
2025-08-16 18:36:18 +0530

    Updated the navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,22 @@
+import Lottie from 'lottie-react';
+import laptopAnimation from '../utils/laptopAnimation';
+
 const NavBar = () => {
+    const styles = { 
+        height: 170, 
+        width: 170, 
+        paddingTop: 10, 
+        paddingLeft: 30 
+    }
+
     return (
         <div className="NavBar">
-            <div className="logo-container">
-                <img className="logo" src="/laptop.gif" alt="Laptop and coffee levitating" />
+            <div className="logo-container"> 
+                <Lottie 
+                    animationData={laptopAnimation} 
+                    loop={true}
+                    style={styles} 
+                />
             </div>
             <div className="btn-container">
                 <button className="btn">Home</button>
