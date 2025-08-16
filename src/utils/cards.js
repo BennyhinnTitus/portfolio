7735dd87b293180717613ed97c73913d5130d3d7
BennyhinnTitus <bennyhinntitus@gmail.com>
2025-08-16 18:41:22 +0530

    Added animation and boosted much styles for About component

---
+++ src/utils/cards.js
@@ -0,0 +1,16 @@
+const cards = [
+    {
+        title: "Projects",
+        followUp: "Developed 5+ Secure Web Apps"
+    },
+    {
+        title: "Hackathon",
+        followUp: "Participated in Adobe India"
+    },
+    {
+        title: "Internship",
+        followUp: "Explored Cloud Security with Azure"
+    }
+]
+
+export default cards
