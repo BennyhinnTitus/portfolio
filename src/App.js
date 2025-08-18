24c4c46a29949bb32260331291ca8adab0a15da5
BennyhinnTitus <bennyhinntitus@gmail.com>
2025-08-18 22:25:59 +0530

    Added more styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './utils/fontAwesome';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import About from './components/About';
+import Skills from './components/Skills';
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
             <NavBar />
             <Home />
             <About />
+            <Skills />
         </div>
     );
 }
